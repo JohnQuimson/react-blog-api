cda3ce11e13165d38b956f39109f886db42aaf53
JohnQuimson <john.quimson777@gmail.com>
2024-06-20 15:33:46 +0200

    feat: chiamata api

--- src/components/ElencoArticles.jsx
+++ src/components/ElencoArticles.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const apiUrl = import.meta.env.VITE_BASE_API_URL;
 
 const ElencoArticles = () => {
   const [articles, setArticles] = useState(null);
 
-  return <>ElencoArticles</>;
+  const fetchArticles = async () => {
+    const { data } = await axios.get(`${apiUrl}/posts`);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  return (
+    <>
+      <div className="articles">
+        {articles === null && 'Loading...'}
+        {articles?.length === 0 && 'No articles found'}
+      </div>
+    </>
+  );
 };
 
 export default ElencoArticles;
